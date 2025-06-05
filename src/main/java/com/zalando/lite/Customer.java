d794d7a33dfee22024209544daeda5f96ab46752
ochwada <nakhulolinda@yahoo.com>
2025-06-05 14:48:33 +0200

    ÂÂCustomer class and method

--- src/main/java/com/zalando/lite/Customer.java
+++ src/main/java/com/zalando/lite/Customer.java
@@ -1,13 +1,17 @@
 package com.zalando.lite;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Represents a customer in the ZalandoLite system.
- *
+ * <p>
  * A customer can place orders, leave reviews, and receive discounts if marked as a VIP.
  * This class includes fields for identity, email, VIP status, and shopping preferences.
- *
+ * <p>
  * Will be used with annotations (@VIP) and linked to orders and reviews.
- *
+ * <p>
  * Concepts reinforced:
  * - Class design & encapsulation
  * - Lists (favorite categories)
@@ -38,40 +42,68 @@ public class Customer {
      */
 
     // Returns the customer's ID
-    public int getId() { /* ... */ }
+    public int getId() {
+        return id;
+    }
 
     // Sets the customer's ID
-    public void setId(int id) { /* ... */ }
+    public void setId(int id) {
+        this.id = id;
+    }
 
     // Returns the name of the customer
-    public String getName() { /* ... */ }
+    public String getName() {
+        return name;
+    }
 
     // Sets the customer's name
-    public void setName(String name) { /* ... */ }
+    public void setName(String name) {
+        this.name = name;
+    }
 
     // Returns the customer's email address
-    public String getEmail() { /* ... */ }
+    public String getEmail() {
+        return email;
+    }
 
     // Sets the customer's email
-    public void setEmail(String email) { /* ... */ }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     // Returns whether the customer is a VIP
-    public boolean isVip() { /* ... */ }
+    public boolean isVip() {
+        return isVip;
+    }
 
     // Sets VIP status (should match the @VIP field annotation logic)
-    public void setVip(boolean vip) { /* ... */ }
+    public void setVip(boolean vip) {
+        this.isVip = isVip;
+    }
 
     // Returns the list of favorite categories
-    public List<String> getFavoriteCategories() { /* ... */ }
+    public List<String> getFavoriteCategories() {
+        return Collections.unmodifiableList(favoriteCategories);
+    }
 
     // Sets the list of favorite categories
-    public void setFavoriteCategories(List<String> categories) { /* ... */ }
+    public void setFavoriteCategories(List<String> categories) {
+        this.favoriteCategories = new ArrayList<>(categories);
+    }
 
     /**
      * Returns a printable summary of the customer's details.
-     *
+     * <p>
      * Useful for debugging, console menus, and reporting.
      */
     @Override
-    public String toString() { /* ... */ }
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", isVip=" + isVip +
+                ", favoriteCategories=" + favoriteCategories +
+                '}';
+    }
 }
